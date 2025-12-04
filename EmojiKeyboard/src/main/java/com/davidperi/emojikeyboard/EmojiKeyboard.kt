69dfbdf78a2f2134ab9e56b896f100c9f5f41681
davidimbox <david.perianez@imbox.me>
2025-12-04 11:44:09 +0100

    refactor: added getters for the layout and improved layout logic.

--- EmojiKeyboard/src/main/java/com/davidperi/emojikeyboard/EmojiKeyboard.kt
+++ EmojiKeyboard/src/main/java/com/davidperi/emojikeyboard/EmojiKeyboard.kt
@@ -2,14 +2,25 @@ package com.davidperi.emojikeyboard
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.LinearLayout
+import com.davidperi.emojikeyboard.databinding.EmojiKeyboardPopupBinding
 
 class EmojiKeyboard @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
 ) : FrameLayout(context, attrs) {
 
-    init {
-        inflate(context, R.layout.emoji_keyboard_popup, this)
-    }
+    private val binding =
+        EmojiKeyboardPopupBinding.inflate(LayoutInflater.from(context), this, true)
+
+    val searchBar: EditText
+        private set
+        get() = binding.searchBar.searchBar
+
+    val topBar: LinearLayout
+        private set
+        get() = binding.topBar
 }
