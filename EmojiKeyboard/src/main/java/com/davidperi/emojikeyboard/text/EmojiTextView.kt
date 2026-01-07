324964c71ba6ac723db673223e1793084d403121
davidimbox <david.perianez@imbox.me>
2026-01-07 17:44:40 +0100

    feature: added possibility to change emoji size.

--- EmojiKeyboard/src/main/java/com/davidperi/emojikeyboard/text/EmojiTextView.kt
+++ EmojiKeyboard/src/main/java/com/davidperi/emojikeyboard/text/EmojiTextView.kt
@@ -6,6 +6,8 @@ import android.text.SpannableStringBuilder
 import android.util.AttributeSet
 import android.util.TypedValue
 import androidx.annotation.CallSuper
+import androidx.annotation.DimenRes
+import androidx.annotation.Px
 import androidx.appcompat.widget.AppCompatTextView
 import com.davidperi.emojikeyboard.ui.span.EmojiTypefaceSpan
 import com.davidperi.emojikeyboard.utils.EmojiFontManager
@@ -21,16 +23,19 @@ open class EmojiTextView @JvmOverloads constructor(
 
     private val emojiTypeface by lazy { EmojiFontManager.getTypeface(context) }
     private var originalTextSize: Float = textSize
+    private var emojiSize: Float = textSize
     private var isUpdating = false
 
     init {
         originalTextSize = textSize
+        emojiSize = textSize
     }
 
     override fun setTextSize(unit: Int, size: Float) {
         super.setTextSize(unit, size)
         if (!isUpdating) {
             originalTextSize = textSize
+            emojiSize = textSize
         }
     }
 
@@ -43,38 +48,43 @@ open class EmojiTextView @JvmOverloads constructor(
 
         isUpdating = true
         try {
-            if (enableDynamicEmojiSize) {
-                updateEmojiSize(text)
-            }
-
             val builder = SpannableStringBuilder(text)
-            replaceEmojis(builder)
+            val targetSpanSize = calculateStylesAndGetSpanSize(text)
+            replaceEmojis(builder, targetSpanSize)
             super.setText(builder, type)
         } finally {
             isUpdating = false
         }
     }
 
-    private fun updateEmojiSize(text: CharSequence) {
+
+    private fun calculateStylesAndGetSpanSize(text: CharSequence): Float? {
+        if (!enableDynamicEmojiSize) {
+            super.setTextSize(TypedValue.COMPLEX_UNIT_PX, originalTextSize)
+            return emojiSize
+        }
+
         val info = text.getEmojiInfo()
 
-        val scaleFactor = if (info.isOnlyEmojis) {
-            when (info.numEmojis) {
+        if (info.isOnlyEmojis) {
+            val scaleFactor = when (info.numEmojis) {
                 1 -> 2.0f
                 2 -> 1.5f
                 3 -> 1.2f
                 else -> 1.0f
             }
+
+            val jumboSize = originalTextSize * scaleFactor
+            super.setTextSize(TypedValue.COMPLEX_UNIT_PX, jumboSize)
+            return null
+
         } else {
-            1.0f
+            super.setTextSize(TypedValue.COMPLEX_UNIT_PX, originalTextSize)
+            return emojiSize
         }
-
-        val newSizePx = originalTextSize * scaleFactor
-
-        super.setTextSize(TypedValue.COMPLEX_UNIT_PX, newSizePx)
     }
 
-    private fun replaceEmojis(spannable: Spannable) {
+    private fun replaceEmojis(spannable: Spannable, spanSizePx: Float?) {
         val existingSpans = spannable.getSpans(0, spannable.length, EmojiTypefaceSpan::class.java)
         for (span in existingSpans) {
             spannable.removeSpan(span)
@@ -83,12 +93,24 @@ open class EmojiTextView @JvmOverloads constructor(
         val matcher = EmojiUtils.EMOJI_PATTERN.matcher(spannable)
         while (matcher.find()) {
             spannable.setSpan(
-                EmojiTypefaceSpan(emojiTypeface),
+                EmojiTypefaceSpan(emojiTypeface, spanSizePx),
                 matcher.start(),
                 matcher.end(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
             )
         }
+    }
+
+
+    fun setEmojiSize(@Px pixels: Int, shouldInvalidate: Boolean = false) {
+        emojiSize = pixels.toFloat()
+        if (shouldInvalidate) {
+            text = text
+        }
+    }
+
+    fun setEmojiSizeRes(@DimenRes res: Int, shouldInvalidate: Boolean = false) {
+        setEmojiSize(resources.getDimensionPixelSize(res), shouldInvalidate)
     }
 
 }
